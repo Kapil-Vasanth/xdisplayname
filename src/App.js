837488517cec22b4bb530c274c3d04640c327571
kapilvasanth <kapilvasanthbalerao@gmail.com>
2024-05-12 22:06:21 +0530

    Testing Form Submission

--- src/App.js
+++ src/App.js
@@ -1,36 +1,43 @@
 import { useState } from 'react';
 import './App.css';
 
-
 function App() {
-  const [name,setName] = useState({firstName:'',lastName:''})
-  const [showName,setShowName] = useState(false);
-  
+  const [name, setName] = useState({ firstName: '', lastName: '' });
+  const [showName, setShowName] = useState(false);
+
   const handleSubmit = (e) => {
-    
-    if(name.firstName && name.lastName)
-    setShowName(true)
-  }
+    e.preventDefault();
+    if (name.firstName && name.lastName) {
+      setShowName(true);
+    }
+  };
 
   const handleName = (e) => {
-   setName(prev => {
-    return {
-      ...prev,
-      [e.target.name]:e.target.value
-    }
-   })
-  }
-
+    setName((prev) => {
+      return {
+        ...prev,
+        [e.target.name]: e.target.value.trim(), // Trim whitespace from input
+      };
+    });
+  };
 
   return (
     <div className="App">
-      <form onSubmit={e => handleSubmit(e)} >
+      <form onSubmit={handleSubmit}>
         <h1>Full Name Display</h1>
-        <label>First Name:</label><input type='text' name='firstName' required onChange={e => handleName(e)}/> <br/>
-        <label>Last Name:</label><input type='text' name='lastName' required onChange={e => handleName(e)}/> <br />
-        <button type='submit'>Submit</button>
+        <label>First Name:</label>
+        <input type="text" name="firstName" required onChange={handleName} />
+        <br />
+        <label>Last Name:</label>
+        <input type="text" name="lastName" required onChange={handleName} />
+        <br />
+        <button type="submit">Submit</button>
       </form>
-      {showName && <p>Full Name : {name.firstName} {name.lastName}</p>}
+      {showName && (
+        <p>
+          Full Name : {name.firstName} {name.lastName}
+        </p>
+      )}
     </div>
   );
 }
